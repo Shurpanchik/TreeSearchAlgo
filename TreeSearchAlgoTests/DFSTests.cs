f4fd61e2d9a2e939b82cd628b573caba54c3c88b
Татьяна Шур <tshur@webzavod.ru>
2021-06-14 18:57:46 +0300

    Еще по одному тесту

--- TreeSearchAlgoTests/DFSTests.cs
+++ TreeSearchAlgoTests/DFSTests.cs
@@ -67,5 +67,30 @@ namespace TreeSearchAlgoTests
             Assert.AreEqual(search.DFS(top, 4), null);
         }
 
+        /// <summary>
+        /// У одной вершины 2 родителей
+        /// </summary>
+        [TestMethod]
+        public void DFSTestTwoParent()
+        {
+            Tree<int> tree = new Tree<int>();
+            TreeNode<int> top = new TreeNode<int>(22);
+            tree.Add(null, top);
+            // добавляем детей 
+            TreeNode<int> child1 = new TreeNode<int>(2);
+            TreeNode<int> child2 = new TreeNode<int>(3);
+            TreeNode<int> child3 = new TreeNode<int>(1);
+            TreeNode<int> child4 = new TreeNode<int>(4);
+            tree.Add(top, child1);
+            tree.Add(child1, child2);
+            tree.Add(child1, child3);
+            tree.Add(child2, child4);
+            tree.Add(child3, child4);
+            TreeSearch<int> search = new TreeSearch<int>();
+            // найден элемент
+            Assert.AreEqual(search.DFS(top, 1).Value, 1);
+            // не найден элемент
+            Assert.AreEqual(search.DFS(top, 4), null);
+        }
     }
 }
