10c2087a3edbced04dc419991ab78587f25f542c
Татьяна Шур <tshur@webzavod.ru>
2021-06-14 11:06:56 +0300

    Добавьте файлы проекта.

---
+++ TreeSearchAlgo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TreeSearchAlgo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
